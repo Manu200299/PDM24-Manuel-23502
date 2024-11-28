2574358e28316cabda9c3fb78b3c1e381960e362
Manuel Fernandes <a23502@alunos.ipca.pt>
2024-11-28 16:31:21 +0000

    News Listing improved plus added thumbnail. Developing news detail...

--- API NY Times/app/src/main/java/com/example/api_ny_times/presentation/coin_list/NewsListScreen.kt
+++ API NY Times/app/src/main/java/com/example/api_ny_times/presentation/coin_list/NewsListScreen.kt
@@ -1,10 +1,14 @@
 package com.example.api_ny_times.presentation.coin_list
 
 import android.graphics.Paint.Align
+import android.os.Build
 import android.util.Log
+import android.widget.Space
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,6 +16,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -31,11 +36,20 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
 import com.example.api_ny_times.domain.model.News
+import java.time.Instant
+import java.time.temporal.ChronoUnit
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun NewsListScreen(
     viewModel: NewsListViewModel = viewModel(),
@@ -47,23 +61,27 @@ fun NewsListScreen(
     LaunchedEffect(Unit) {
         viewModel.fetchNews()
     }
+    // Main Column
     Column(
         modifier = Modifier
             .background(Color.White)
             .fillMaxWidth()
     ) {
+        // Top Header
         Row(
             modifier = Modifier
                 .background(color = headerCustomColor)
                 .fillMaxWidth()
                 .padding(16.dp)
         ) {
+            // Top Header Text
             Text(
                 text = "Recent News",
                 color = Color.White,
                 fontWeight = FontWeight.Bold,
                 style = MaterialTheme.typography.headlineSmall)
         }
+        // Column that will lsit the news
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
@@ -77,42 +95,64 @@ fun NewsListScreen(
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
+@OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun NewsItem(news: News, onClick: () -> Unit){
     var isClicked by remember { mutableStateOf(false) }
 
+    // News List Card
     Card(
         modifier = Modifier
+            .clickable { onClick() }
             .fillMaxWidth()
             .background(Color.White)
-            .padding(8.dp),
-        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+            .padding(10.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
     ){
-        Row(
-            modifier = Modifier
-                .padding(16.dp)
-                .fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Spacer(modifier = Modifier.width(16.dp))
+        // News List
+        Column {
+            // UrlToImage thumbnail
+            GlideImage(
+                model = news.urlToImage,
+                contentDescription = "NewsThumbnail",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp),
+                contentScale = ContentScale.Crop
+            )
 
             Column(
                 modifier = Modifier
-                    .weight(1f)
-                    .clickable { onClick() }
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
             ) {
+                // News Title
                 Text(
                     text = news.title,
                     style = MaterialTheme.typography.titleMedium,
-                    fontWeight = FontWeight.Bold
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
                 )
-                Text(
-                    text = news.name,
-                    style = MaterialTheme.typography.bodyLarge,
-                    fontWeight = FontWeight.Bold
-                )
-            }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = news.name,
+                        style = MaterialTheme.typography.bodySmall,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
+                    )
 
+                    Text(
+//                        text = "${ChronoUnit.MINUTES.between(news.publishedAt, Instant.now())} minutes ago",
+                        text = "Published at ${news.publishedAt}",
+                        style = MaterialTheme.typography.bodySmall,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
+                    )
+                }
+            }
         }
     }
 }
